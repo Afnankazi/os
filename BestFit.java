8361b973481f0468be69e4c2a34e2e3d0691d500
Jay Kerkar <bytewise0405@gmail.com>
2025-04-28 14:21:48 +0000

    Add memory allocation algorithms: Best-Fit, First-Fit, and Worst-Fit implementations

---
+++ BestFit.java
@@ -0,0 +1,43 @@
+public class BestFit {
+    public static void main(String[] args) {
+        int[] blockSize = {100, 500, 200, 300, 600};
+        int[] processSize = {212, 417, 112, 426};
+        
+        int m = blockSize.length;
+        int n = processSize.length;
+        
+        int[] allocation = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            allocation[i] = -1;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            int bestIdx = -1;
+            for (int j = 0; j < m; j++) {
+                if (blockSize[j] >= processSize[i]) {
+                    if (bestIdx == -1 || blockSize[j] < blockSize[bestIdx]) {
+                        bestIdx = j;
+                    }
+                }
+            }
+            
+            if (bestIdx != -1) {
+                allocation[i] = bestIdx;
+                blockSize[bestIdx] -= processSize[i];
+            }
+        }
+        
+        System.out.println("Best-Fit Algorithm:");
+        System.out.println("Process No.\tProcess Size\tBlock No.");
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print((i + 1) + "\t\t" + processSize[i] + "\t\t");
+            if (allocation[i] != -1) {
+                System.out.println(allocation[i] + 1);
+            } else {
+                System.out.println("Not Allocated");
+            }
+        }
+    }
+}
